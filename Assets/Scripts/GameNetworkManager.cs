5aea71e3c3552ede22f725d55f6b8c646a21f50a
Kevin Wu <contact@kevinwu.io>
2020-08-11 20:33:28 -0700

    Added a ton of new assets and basic server.

---
+++ Assets/Scripts/GameNetworkManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class GameNetworkManager : NetworkManager
+{
+    public Grid grid;
+    public GameObject map;
+
+    override public void OnStartServer() {
+        Instantiate(map, Vector2.zero, Quaternion.identity, grid.transform);
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# shootyshoot
+
+A simple tactical TBS that I'm working on.
+
+- [Design Doc](https://docs.google.com/document/d/1bk-_awlNdstk4vl2o0oCZ7COe61VNm-oX6tgVheXvdM/edit)
+
